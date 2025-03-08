00c92b3d9d1d4e49b9073911175fe6b03b11306f
Jini <wlsdl6685@gmail.com>
2025-03-08 22:52:22 +0900

    feat: Detail 페이지 구성
    - 뒤로가기 버튼, 상세 정보, 댓글 컴포넌트로 구성
    - #8

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,35 @@
+import Comments from '@/components/Detail/Comments';
 import DetailView from '@/components/Detail/DetailView';
+import BackButton from '@/components/Common/BackButton';
 
 const Detail = () => {
-  return <DetailView />;
+  return (
+    <>
+      <DetailStyle>
+        <BackButton />
+        <DetailView />
+        <Divider />
+        <Comments />
+      </DetailStyle>
+    </>
+  );
 };
+
+import styled from 'styled-components';
+
+const DetailStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 10px;
+  gap: 20px;
+`;
+
+const Divider = styled.hr`
+  width: 100%;
+  height: 1px;
+  background-color: #ddd;
+  margin: 20px 0;
+  border: none;
+`;
 
 export default Detail;
