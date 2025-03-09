bcc5d3a06109c6f9dc6da88fa922c39187ae885e
Jini <wlsdl6685@gmail.com>
2025-03-09 21:20:26 +0900

    feat: 뒤로가기 버튼 스타일링 수정
    - #10

--- src/components/Common/BackButton.tsx
+++ src/components/Common/BackButton.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@/style/Theme';
 import { FaArrowLeftLong } from 'react-icons/fa6';
+import styled from 'styled-components';
 
 const BackButton = () => {
   const handleClick = () => {
@@ -7,12 +8,19 @@ const BackButton = () => {
   };
 
   return (
-    <FaArrowLeftLong
-      size="1rem"
-      color={`${Theme.colors.mainText}`}
-      onClick={handleClick}
-    />
+    <BackButtonStyle>
+      <FaArrowLeftLong
+        size="1rem"
+        color={`${Theme.colors.mainText}`}
+        onClick={handleClick}
+      />
+    </BackButtonStyle>
   );
 };
 
+const BackButtonStyle = styled(FaArrowLeftLong)`
+  margin-top: 10px;
+  margin-left: 10px;
+  cursor: pointer;
+`;
 export default BackButton;
