fa4414905ff99c64c4cc2489bfd0fac8eb1a8089
Jaewon <wellbird@naver.com>
2025-03-11 16:08:56 +0900

    feat : 카카오 맵 상에 현위치 기능 종합
    - 현위치 마커 생성
    - 현위치 버튼 생성
    - #7

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import useKakaoLoader from '@/hooks/useKakaoLoader';
+import CurrentLocationButton from '@/components/Main/CurrentLocationButton';
+import { ILocation } from '@/types';
+import styled from 'styled-components';
+import MyLocation from '@/components/Main/MyLocation';
+import useLocationStore from '@/store/locationStore';
+import useCurrentLocation from '@/hooks/useCurrentLocation';
 
-function Home() {
+const Home = () => {
+  const mapRef = useRef<kakao.maps.Map>(null);
+  const location = useLocationStore.getState().location;
+  const errorCode = useLocationStore((state) => state.errorCode);
+  const { getLocation } = useCurrentLocation();
+  const [center, setCenter] = useState<ILocation>(location);
+
   useKakaoLoader();
+
+  useEffect(() => {
+    const getInitialLocation = async () => {
+      await getLocation();
+      const location = useLocationStore.getState().location;
+      setCenter(location);
+    };
+    getInitialLocation();
+  }, []);
+
+  const handleIdle = (map: kakao.maps.Map) => {
+    const newCenter = map.getCenter();
+    const latitude = newCenter.getLat();
+    const longitude = newCenter.getLng();
+    setCenter({ latitude, longitude });
+  };
+
   return (
-    <Map
-      id="map"
-      center={{
-        lat: 33.450701, // 기준 좌표 현위치 개발 시 수정
-        lng: 126.570667, // 기준 좌표 현위치 개발 시 수정
-      }}
-      style={{
-        width: '100%',
-        height: '100%',
-      }}
-      level={3}
-    />
+    <HomeStyle>
+      <Map
+        id="map"
+        center={{
+          lat: center.latitude,
+          lng: center.longitude,
+        }}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        level={3}
+        ref={mapRef}
+        onIdle={handleIdle}
+        isPanto={true}
+      >
+        {errorCode === null && <MyLocation />}
+      </Map>
+      <CurrentLocationButton setCenter={setCenter} />
+    </HomeStyle>
   );
-}
+};
 
 export default Home;
+
+const HomeStyle = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
