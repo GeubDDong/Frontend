c0bebe803c74b1d54d4719068264b1299b4835b1
Jini <wlsdl6685@gmail.com>
2025-03-14 16:06:59 +0900

    feat: 좋아요 버튼 옆에 개수 표시 UI 추가
    - #17

--- src/components/Detail/LikeButton.tsx
+++ src/components/Detail/LikeButton.tsx
@@ -1,6 +1,7 @@
 import { Theme } from '@/style/Theme';
 import { useState } from 'react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa6';
+import styled from 'styled-components';
 
 const LikeButton = () => {
   const [isLike, setIsLike] = useState(false);
@@ -9,23 +10,33 @@ const LikeButton = () => {
   };
   return (
     <>
-      {isLike ? (
-        <FaHeart
-          size="1.5rem"
-          color="#eb3b41" // FF4033
-          onClick={handleClickLike}
-          style={{ cursor: 'pointer' }}
-        />
-      ) : (
-        <FaRegHeart
-          size="1.5rem"
-          color={`${Theme.colors.subText}`}
-          onClick={handleClickLike}
-          style={{ cursor: 'pointer' }}
-        />
-      )}
+      <LikeButtonStyle>
+        {isLike ? (
+          <FaHeart
+            size="1.5rem"
+            color="#eb3b41" // FF4033
+            onClick={handleClickLike}
+            style={{ cursor: 'pointer' }}
+          />
+        ) : (
+          <FaRegHeart
+            size="1.5rem"
+            color={`${Theme.colors.subText}`}
+            onClick={handleClickLike}
+            style={{ cursor: 'pointer' }}
+          />
+        )}
+        <span>0</span>
+      </LikeButtonStyle>
     </>
   );
 };
 
+const LikeButtonStyle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  font-size: ${Theme.fontSize.sm};
+  color: ${Theme.colors.subText};
+`;
 export default LikeButton;
