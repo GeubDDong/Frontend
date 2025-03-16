df7949b656bc42e318865214e82f7aeac362590a
Jini <wlsdl6685@gmail.com>
2025-03-16 20:59:37 +0900

    feat: 기본정보 컴포넌트 추가
    - 기본정보 컴포넌트
    - 현재 화장실 정보 상태관리
    - mock data 추가 작성
    - #17

--- src/mocks/mockData.ts
+++ src/mocks/mockData.ts
@@ -1,7 +1,35 @@
 import { ICommentItem } from '@/components/Detail/Comments';
-import { IDetailView } from '@/components/Detail/DetailView';
+import { IToiletDetailInfo } from '@/components/Detail/DetailView';
+import { IToiletBasicInfo } from '@/components/Main/ToiletBasicInfo';
+import { IToiletInfo } from '@/types';
 
-export const mockToiletDetail: IDetailView = {
+export const mockToiletInfo: IToiletInfo = {
+  id: 1,
+  name: '만석공원',
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+  },
+  street_address: '수원시 장안구 정조로1087(송죽동, 만석공원)',
+  lot_address: '지번 주소',
+  open_hours: '09:00 - 22:00',
+};
+
+export const mockToiletBasicInfo: IToiletBasicInfo = {
+  id: 1,
+  name: '만석공원',
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+  },
+  street_address: '수원시 장안구 정조로1087(송죽동, 만석공원)',
+  lot_address: '지번 주소',
+  open_hours: '09:00 - 22:00',
+  liked: true,
+  nearest: true,
+};
+
+export const mockToiletDetail: IToiletDetailInfo = {
   id: 1,
   name: '만석공원',
   street_address: '수원시 장안구 정조로1087(송죽동, 만석공원)',
@@ -12,9 +40,11 @@ export const mockToiletDetail: IDetailView = {
   kids_toilet_female: 'N',
   management_agency: '장안구 공원녹지과',
   phone_number: '010-1234-5678',
-  open_hour: '09:00 - 22:00',
-  latitude: 37.5665,
-  longitude: 126.978,
+  open_hours: '09:00 - 22:00',
+  location: {
+    latitude: 37.5665,
+    longitude: 126.978,
+  },
   emergency_bell: 'N',
   cctv: 'Y',
   diaper_changing_station: 'N',
@@ -37,46 +67,25 @@ export const mockComments: ICommentItem[] = [
     comment: '깨끗한 화장실이에요!',
     updated_at: '2025-03-06T12:34:56Z',
   },
-  {
-    id: 2,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
-  {
-    id: 3,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
-  {
-    id: 4,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
-  {
-    id: 5,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
-  {
-    id: 5,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
-  {
-    id: 5,
-    user_email: 'user123@naver.com',
-    nickname: '철수짱',
-    comment: '깨끗한 화장실이에요!',
-    updated_at: '2025-03-06T12:34:56Z',
-  },
+  // {
+  //   id: 2,
+  //   user_email: 'user123@naver.com',
+  //   nickname: '철수짱',
+  //   comment: '깨끗한 화장실이에요!',
+  //   updated_at: '2025-03-06T12:34:56Z',
+  // },
+  // {
+  //   id: 3,
+  //   user_email: 'user123@naver.com',
+  //   nickname: '철수짱',
+  //   comment: '깨끗한 화장실이에요!',
+  //   updated_at: '2025-03-06T12:34:56Z',
+  // },
+  // {
+  //   id: 4,
+  //   user_email: 'user123@naver.com',
+  //   nickname: '철수짱',
+  //   comment: '깨끗한 화장실이에요!',
+  //   updated_at: '2025-03-06T12:34:56Z',
+  // },
 ];
