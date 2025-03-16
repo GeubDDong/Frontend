c300d6b8ecee9c0adfe332a4407769441f1ef091
Jini <wlsdl6685@gmail.com>
2025-03-16 20:54:23 +0900

    feat: 화장실정보 인터페이스 추가
    - #17

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ export interface ILocation {
   latitude: number;
   longitude: number;
 }
+
+export interface IToiletInfo {
+  id: number;
+  name: string;
+  location: ILocation;
+  street_address: string;
+  lot_address: string;
+  open_hours: string;
+}
