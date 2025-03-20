cb67f7e776e31223eff38cd57b51eb15277d391d
Jaewon <wellbird@naver.com>
2025-03-20 20:25:50 +0900

    style : 카카오 기본 마커 적용
    - #32

--- src/components/Home/ToiletMarker.tsx
+++ src/components/Home/ToiletMarker.tsx
@@ -1,6 +1,4 @@
 import { MapMarker } from 'react-kakao-maps-sdk';
-import locationPin_liked from '@/assets/locationPin_liked.svg';
-import locationPin_normal from '@/assets/locationPin_normal.svg';
 import useToiletInfoStore from '@/store/toiletInfoStore';
 import { IToiletBasicInfo } from '@/models/toiletBasicInfo.model';
 
@@ -9,19 +7,19 @@ interface IToiletMarkerProps {
 }
 
 const ToiletMarker = ({ info }: IToiletMarkerProps) => {
-  const selectedToiletDataInfo = useToiletInfoStore((state) => state.info);
+  // const selectedToiletDataInfo = useToiletInfoStore((state) => state.info);
   const setSelectedToiletDataInfo = useToiletInfoStore(
     (state) => state.setInfo,
   );
   return (
     <MapMarker
-      image={{
-        src: info.liked.like ? locationPin_liked : locationPin_normal,
-        size:
-          selectedToiletDataInfo && selectedToiletDataInfo.id === info.id
-            ? { width: 40, height: 40 }
-            : { width: 30, height: 30 },
-      }}
+      // image={{
+      //   src: info.liked.like ? locationPin_liked : locationPin_normal,
+      //   size:
+      //     selectedToiletDataInfo && selectedToiletDataInfo.id === info.id
+      //       ? { width: 40, height: 40 }
+      //       : { width: 25, height: 25 },
+      // }}
       position={{ lat: info.latitude, lng: info.longitude }}
       onClick={() => setSelectedToiletDataInfo(info)}
     />
