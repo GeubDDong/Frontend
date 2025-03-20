092c1c9dcd2237fca6e6b545949d11eab0a2516a
Jaewon <wellbird@naver.com>
2025-03-20 15:19:29 +0900

    feat : 메인 지도 화장실 정보 응답 모델 작성
    - #26

---
+++ src/model/mainToiletInfo.model.ts
@@ -0,0 +1,14 @@
+export interface mainToiletInfoModel {
+  id: number;
+  latitude: number;
+  longitude: number;
+  liked: {
+    like: boolean;
+    count: number;
+  };
+  lot_address: string;
+  name: string;
+  nearest: boolean;
+  open_hours: string;
+  street_address: string;
+}
