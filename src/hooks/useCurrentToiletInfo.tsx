08774851e788162fd2f2009fe1dc1cb3b8078866
Jaewon <wellbird@naver.com>
2025-04-02 00:51:01 +0900

    refactor : setSelectedToiletInfo 추가
    - #47

--- src/hooks/useCurrentToiletInfo.tsx
+++ src/hooks/useCurrentToiletInfo.tsx
@@ -2,6 +2,7 @@ import useToiletInfoStore from '@/store/toiletInfoStore';
 
 export const useCurrentToiletInfo = () => {
   const info = useToiletInfoStore((state) => state.info);
+  const setSelectedToiletInfo = useToiletInfoStore((state) => state.setInfo);
 
-  return { toiletId: info?.id };
+  return { toiletId: info?.id, setSelectedToiletInfo };
 };
