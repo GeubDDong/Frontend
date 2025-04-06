ee4aae40c26dcf232295eca1f3c2c762dbffdb21
Jaewon <wellbird@naver.com>
2025-04-06 21:57:59 +0900

    feat : 마이페이지, 랭킹 라우팅용 빈 화면 생성
    - #49

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MyPage = () => {
+  return <MyPageStyle>MyPage</MyPageStyle>;
+};
+
+export default MyPage;
+
+const MyPageStyle = styled.div`
+  height: calc(100% - 60px);
+  width: 100%;
+`;
---
+++ src/pages/Rank.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Rank = () => {
+  return <RankStyle>Rank</RankStyle>;
+};
+
+export default Rank;
+
+const RankStyle = styled.div`
+  height: calc(100% - 60px);
+  width: 100%;
+`;
