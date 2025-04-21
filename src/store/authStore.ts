46857504111dd0cf526241c5ba72db642cff308b
Jini <wlsdl6685@gmail.com>
2025-04-21 14:28:10 +0900

    refact : 카카오 로그아웃 api 미사용으로 변경
    - #56

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,24 +1,17 @@
 import { logout } from '@/api/auth.api';
-import { TLoginProvider } from '@/types';
 import { create } from 'zustand';
 
 interface IUseAuthStore {
   isLogin: boolean;
-  logout: (provider: TLoginProvider) => void;
+  logout: () => void;
 }
 
 export const useAuthStore = create<IUseAuthStore>((set) => ({
   isLogin: false,
-  logout: async (provider: TLoginProvider) => {
+  logout: async () => {
     try {
       await logout();
       set({ isLogin: false });
-
-      switch (provider) {
-        case 'kakao': {
-          window.location.href = `https://kauth.kakao.com/oauth/logout?client_id=${import.meta.env.VITE_KAKAO_OAUTH_API_KEY}&logout_redirect_uri=http://localhost:3000/`;
-        }
-      }
     } catch (error) {
       console.log(error);
     }
