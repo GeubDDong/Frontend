57198e9e7fcdf986e30332acc66ac44da7dd177c
Jaewon <wellbird@naver.com>
2025-04-21 12:05:28 +0900

    fix : placeService 오류 수정
    - #54

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,13 +1,17 @@
-import kakaoPlacesService from '@/utils/kakaoPlacesService';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const useSearch = () => {
   const [searchResult, setSearchResult] =
     useState<kakao.maps.services.PlacesSearchResult>([]);
+  const psRef = useRef<kakao.maps.services.Places>();
+
+  useEffect(() => {
+    psRef.current = new kakao.maps.services.Places();
+  }, []);
 
   const searchAddress = (keyword: string) => {
-    const ps = kakaoPlacesService;
-    ps.keywordSearch(keyword, (data, status) => {
+    if (!psRef.current) return;
+    psRef.current.keywordSearch(keyword, (data, status) => {
       if (status === kakao.maps.services.Status.OK) {
         setSearchResult(data);
       }
--- src/utils/kakaoPlacesService.ts
+++
@@ -1,3 +0,0 @@
-const kakaoPlacesService = new kakao.maps.services.Places();
-
-export default kakaoPlacesService;
