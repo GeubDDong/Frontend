621632c37e69bcd5f9e1722894374030a689b913
Jaewon <wellbird@naver.com>
2025-04-28 13:25:45 +0900

    feat : 카드 컨테이너 스크롤 영역 구현
    - #62

---
+++ src/components/MyPage/ScrollList.tsx
@@ -0,0 +1,44 @@
+import { TTabType } from '@/types';
+import styled from 'styled-components';
+import ItemCard from './ItemCard';
+import MyPageModel from '@/models/myPage.model';
+
+interface IScrollListProps {
+  tabType: TTabType;
+  listItems: MyPageModel;
+}
+
+const ScrollList = ({ tabType, listItems }: IScrollListProps) => {
+  return (
+    <ScrollListStyle>
+      {tabType === 'likeList' &&
+        (listItems.favorites.length > 0 ? (
+          listItems.favorites.map((item) => (
+            <ItemCard tabType={tabType} item={item} />
+          ))
+        ) : (
+          <div className="info_message">즐겨찾기한 화장실이 없습니다.</div>
+        ))}
+      {tabType === 'reviewList' &&
+        (listItems.reviews.length > 0 ? (
+          listItems.reviews.map((item) => (
+            <ItemCard tabType={tabType} item={item} />
+          ))
+        ) : (
+          <div className="info_message">작성한 댓글이 없습니다.</div>
+        ))}
+    </ScrollListStyle>
+  );
+};
+
+export default ScrollList;
+
+const ScrollListStyle = styled.div`
+  display: flex;
+  flex: 1;
+  overflow-y: auto;
+  padding: 10px;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+`;
