21201c644fab48a87c1891b84df0429e8da17e8c
Jini <wlsdl6685@gmail.com>
2025-04-30 17:18:38 +0900

    fix :formatRating string 처리

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,3 +1,8 @@
-export const formatRating = (rating: number): string => {
-  return rating.toFixed(1);
+export const formatRating = (rating: number | string): string => {
+  console.log(`type: ${typeof rating} value: ${rating}`);
+  if (typeof rating === 'string') {
+    rating = parseFloat(rating);
+  }
+
+  return rating === 0 ? `${rating}` : rating.toFixed(1);
 };
